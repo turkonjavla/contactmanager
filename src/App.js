68693098f0ddd8a8fbce8066d9fa544aa1eaf1f9
Vladimir <turkonjavlada@gmail.com>
2018-10-24 12:40:27 +0200

    Add Contacts component to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Provider } from './context';
+import Contacts from './components/contacts/Contacts';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Hello world</h2>
-      </div>
+      <Provider>
+        <div className="App">
+          <h2>Hello world</h2>
+          <div className="container">
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
