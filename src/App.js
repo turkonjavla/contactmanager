c5f788827e48f101e1744ce3defc755dab24f417
Vladimir <turkonjavlada@gmail.com>
2018-10-30 15:37:28 +0100

    Add Header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from './context';
 import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <h2>Hello world</h2>
+        <div classNaame="App">
+          <Header branding="Contact Manager" />
           <div className="container">
             <Contacts />
           </div>
