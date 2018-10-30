2b8fc2d0e016340c195cf15db4076d03d0cc34bc
Vladimir <turkonjavlada@gmail.com>
2018-10-30 15:58:16 +0100

    Implement react-router-dom to App..js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Context API
 import { Provider } from './context';
+
+// Components
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 
@@ -9,12 +14,16 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div classNaame="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <Contacts />
+        <Router>
+          <div classNaame="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={ Contacts } />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
