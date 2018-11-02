99c7ea42557975d2d24c4e4d356dca4f5069fdaa
Vladimir <turkonjavlada@gmail.com>
2018-11-02 15:41:33 +0100

    Revert "Implement ADD_CONTACT case to context API"
    This reverts commit d2b07223daf39a4bb1e8f736cf7081673f86474c.

--- src/App.js
+++ src/App.js
@@ -11,19 +11,17 @@ import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
-import AddContact from "./components/contacts/AddContact";
 
 class App extends Component {
   render() {
     return (
       <Provider>
         <Router>
-          <div className="App">
+          <div classNaame="App">
             <Header branding="Contact Manager" />
             <div className="container">
               <Switch>
                 <Route exact path="/" component={ Contacts } />
-                <Route exact path="/contact/add" component={ AddContact } />
                 <Route exact path="/about" component={ About } />
                 <Route component={ NotFound } />
               </Switch>
--- src/context.js
+++ src/context.js
@@ -9,11 +9,6 @@ const reducer = (state, action) => {
         ...state,
         contacts: state.contacts.filter(contact => contact.id !== action.payload)
       };
-    case 'ADD_CONTACT': 
-      return {
-        ...state,
-        contacts: [action.payload, ...state.contacts]
-      }
     default:
       return state;
   }
