6afab809f7e52341f66323778eefff3004fd1649
Vladimir <turkonjavlada@gmail.com>
2018-11-04 12:43:44 +0100

    Fix class -> className

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="row justify-content-left">
       <div className="col-md-6">
         <h3 style={{ color: '#9E9E9E', fontWeight: 'bold' }} className="display-2">404</h3>
-        <p class="lead">We couldn't find the page you were looking for</p>
+        <p className="lead">We couldn't find the page you were looking for</p>
         <Link to="/" style={{ fontWeight: 'bold'}} className="text-muted"><i className="fas fa-chevron-left"></i> Back to Home</Link>
       </div>
     </div>
