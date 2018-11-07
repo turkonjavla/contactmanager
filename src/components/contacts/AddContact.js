0d095ffd2524a56883f3dd897ee305f8b346b74c
Vladimir <turkonjavlada@gmail.com>
2018-11-07 15:26:48 +0100

    Add post request for adding new contacts using axios
    - Removed uuid 
    - Add redirect to main page after adding contact

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
-import uuid from 'uuid';
 import TextInputGroup from '../layout/TextInputGroup';
+import axios from 'axios';
 
 class AddContact extends Component {
   state = {
@@ -54,16 +54,19 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
     };
 
-    dispatch({
-      type: 'ADD_CONTACT',
-      payload: newContact
-    });
+    axios
+      .post('https://jsonplaceholder.typicode.com/users', newContact)
+      .then(res => {
+        dispatch({
+          type: 'ADD_CONTACT',
+          payload: res.data
+        });
+      });
 
     // clear state
     this.setState({
@@ -72,6 +75,8 @@ class AddContact extends Component {
       phone: '',
       errors: {}
     });
+
+    this.props.history.push('/');
   };
 
   render() {
